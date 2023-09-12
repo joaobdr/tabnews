84b57e7725d0049f5b2354e7af1af1434e4a78cd
joaobdr <143668802+joaobdr@users.noreply.github.com>
2023-09-12 01:49:16 +0000

    adciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
-function Home(){
-    return <h1>Pagina em criação...</h1>
+function Home() {
+  return <h1>Pagina em criação...</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
 
-export default Home
+function teste2() {
+  console.log("sadfsdfsf");
+}
+export default Home;
