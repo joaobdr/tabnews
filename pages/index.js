c659f5c940f7a2aeda2fd48c56eb4e9ad791f2d6
joaobdr <143668802+joaobdr@users.noreply.github.com>
2023-09-22 00:47:34 +0000

    atualizacao

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,4 @@ function Home() {
   return <h1>Pagina em criação...</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("sadfsdfsf");
-}
 export default Home;
